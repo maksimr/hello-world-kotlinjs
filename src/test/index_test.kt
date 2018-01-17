b7478db6d0087a6319ffcafdd46184c24441f050
Maksim Ryzhikov <rv.maksim@gmail.com>
2018-01-17 20:56:35 +0300

    chore: add example for async test

--- src/test/index_test.kt
+++ src/test/index_test.kt
@@ -1,7 +1,21 @@
+import kotlin.js.Promise
 import kotlin.test.*
 
+external fun setTimeout(handler: (Unit) -> Unit): dynamic
+
 class IndexTest {
-    @Test fun testVersion() {
+    @Test
+    fun testVersion() {
         assertEquals(version(), "1.0.0")
+    }
+
+    @Test
+    fun testAsync(): Promise<Boolean> {
+        return Promise<Boolean> { resolve, _ ->
+            setTimeout({
+                assertEquals(true, true)
+                resolve(true)
+            })
+        }
     }
 }
